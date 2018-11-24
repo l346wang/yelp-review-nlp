c027e0c6ecd6fe4781f829ac59b4f61cb5667043
ChenxiYuan <yuanchenxi95@gmail.com>
2018-11-24 16:31:05 -0500

    word id

--- wordId.py
+++ wordId.py
@@ -10,7 +10,16 @@ yelp_database = client['yelp']
 
 words_hit_count_collection = yelp_database['words_hit_count']
 
-print(len(words_hit_count_collection.find().distinct('word')))
+idx = 0
+for word in words_hit_count_collection.find().distinct('word'):
+  words_hit_count_collection.update({
+    'word': word
+  }, {
+    '$set': {
+      'index': idx,
+    },
+  }, multi=True, w=0)
+  idx += 1
 
 
 
