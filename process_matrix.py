0cf7dd32f3c3827e9dfe5763c430a252a1fd1f26
ChenxiYuan <yuanchenxi95@gmail.com>
2018-11-25 00:14:57 -0500

    add processed json file

---
+++ process_matrix.py
@@ -0,0 +1,73 @@
+
+from pymongo import MongoClient
+import pymongo
+
+from config import MONGO_DB_ADDRESS
+
+
+client = MongoClient(MONGO_DB_ADDRESS, serverSelectionTimeoutMS=1)
+yelp_database = client['yelp']
+
+words_hit_count_collection = yelp_database['words_hit_count']
+tokenized_reviews_collection = yelp_database['tokenized_reviews2']
+
+import json
+
+MAX_SENTENCE_LENGTH = 200
+
+vocabulary_list = words_hit_count_collection.aggregate([
+  {
+    '$group': {
+      '_id': {
+        'word': '$word',
+        'index': '$index',
+      },
+    }
+  },
+  {
+    '$project': {
+      '_id': 0,
+      'word': '$_id.word',
+      'index': '$_id.index'
+    }
+  }
+])
+
+vocabulary_dict = dict()
+
+for vocab in vocabulary_list:
+  vocabulary_dict[vocab['word']] = vocab['index']
+
+vocab_size = len(vocabulary_dict) + 1
+
+
+tokenized_reviews_list = tokenized_reviews_collection.find()
+
+tokens_as_index_matrix = []
+star_matrix = []
+
+for review in tokenized_reviews_list:
+  index_row = []
+  for word in review['tokens']:
+    index_row.append(vocabulary_dict[word])
+  if len(index_row) < MAX_SENTENCE_LENGTH:
+    length_of_padding = MAX_SENTENCE_LENGTH - len(index_row)
+    index_row.extend([0] * length_of_padding)
+  else:
+    index_row = index_row[:MAX_SENTENCE_LENGTH]
+
+  tokens_as_index_matrix.append(index_row)
+  minus_1_star = review['stars'] - 1
+  star_matrix.append([minus_1_star])
+
+vocab_size = len(vocabulary_dict) + 1
+
+def write_to_file(filename, data):
+  with open(filename, 'w') as the_file:
+    json.dump(data, the_file)
+
+write_to_file('tokens_as_index_matrix.json', tokens_as_index_matrix)
+write_to_file('star_matrix.json', star_matrix)
+write_to_file('vocabulary_size.json', {
+  'vocab_size': vocab_size
+})
---
+++ rnn.py
@@ -0,0 +1,64 @@
+import json
+
+with open('./star_matrix.json') as f:
+  star_matrix = json.load(f)
+
+with open('./tokens_as_index_matrix.json') as f:
+  tokens_as_index_matrix = json.load(f)
+
+with open('./vocabulary_size.json') as f:
+  vocabulary_size_file = json.load(f)
+vocab_size = vocabulary_size_file['vocab_size']
+
+NUMBER_OF_CLASS = 5
+
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+
+
+
+np_token_matrix = np.asarray(tokens_as_index_matrix, dtype=np.int)
+np_label_matrix = np.asarray(star_matrix, dtype=np.int)
+
+model = keras.Sequential()
+# model.add(keras.layers.Embedding(vocab_size, 256))
+# model.add(keras.layers.GlobalAveragePooling1D())
+# model.add(keras.layers.Dense(256, activation=tf.nn.relu))
+# model.add(keras.layers.Dense(10, activation=tf.nn.relu))
+# model.add(keras.layers.Dense(NUMBER_OF_CLASS, activation=tf.nn.softmax))
+model.add(keras.layers.Embedding(vocab_size, 144))
+model.add(keras.layers.GlobalAveragePooling1D())
+model.add(keras.layers.Dense(144, activation=tf.nn.relu))
+model.add(keras.layers.Dense(10, activation=tf.nn.relu))
+model.add(keras.layers.Dense(NUMBER_OF_CLASS, activation=tf.nn.softmax))
+
+model.summary()
+
+model.compile(optimizer=tf.train.AdamOptimizer(),
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+x_val = np_token_matrix[8000:]
+y_val = np_label_matrix[8000:]
+
+test_data = np_token_matrix[:8000]
+test_labels = np_label_matrix[:8000]
+
+history = model.fit(x_val,
+                    y_val,
+                    epochs=10,
+                    batch_size=512,
+                    validation_split=0.2,
+                    verbose=1)
+
+
+model.save('my_model.h5')
+
+
+results = model.evaluate(test_data, test_labels)
+
+print(results)
+predict = model.predict(test_data)
+
+
